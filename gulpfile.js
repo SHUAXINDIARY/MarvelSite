ff53fb0c8bfb09e31dc3aed3253731f0d9327044
shuaxin <986384495@qq.com>
2019-07-03 15:13:23 +0800

    更新

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 let gulp = require('gulp');
 let less = require('gulp-less');
-let del=require('del');
-gulp.task('clean',function(done){
-    del(['src/css/.css'],done());
+let del = require('del');
+// 清除旧文件
+gulp.task('clean', function (done) {
+    del(['src/css/.css'], done());
 });
-gulp.task('less',function(done){
+// 编译less
+gulp.task('less', function (done) {
     gulp.src('src/less/*.less')
         .pipe(less())
-        .pipe(gulp.dest('src/css'))
-});
-gulp.task('watch',function(done){
-    gulp.watch('src/less/*.less',gulp.series('less'));
+        .pipe(gulp.dest('src/css'));
     done();
 });
-gulp.task('start',gulp.series('clean','watch',gulp.parallel('less')),function(done){
+// 监听less文件变化
+gulp.task('watch', function (done) {
+    gulp.watch('src/less/*.less', gulp.series('less'));
+});
+// 启动
+gulp.task('start', gulp.series('clean', gulp.parallel('less', 'watch')), function (done) {
     done();
 });
