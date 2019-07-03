c04faafc5572e5dbb45ac365db78ebf68a262942
shuaxin <986384495@qq.com>
2019-07-03 22:32:04 +0800

    优化

--- src/js/hero.js
+++ src/js/hero.js
@@ -2,17 +2,18 @@ $(function () {
     let dom = {
         li_btn: $('#li_btn'),
         nav_two: $('#nav_two'),
-        ironman: $('#ironman'),
         thor: $('#thor'),
         hulk: $('#hulk'),
         widow: $('#widow'),
-        section:$('#section')
+        section: $('#section'),
+        back_top: $('#back_top')
     };
     // 展开二级菜单
     dom.li_btn.hover(function () {
         dom.nav_two.toggle();
     });
     // 跳转到页面指定区域
+    /**jquery添加事件监听 */
     dom.nav_two.on('click', function (e) {
         let id = e.target.id;
         switch (id) {
@@ -22,14 +23,14 @@ $(function () {
             case 'hulk':
                 $('html,body').animate({ scrollTop: dom.section.innerHeight() * 1.9 }, 500);
                 break;
-            case 'ironman':
-                $('html,body').animate({ scrollTop: dom.section.innerHeight() * 0 }, 500);
-                break;
             case 'widow':
                 $('html,body').animate({ scrollTop: dom.section.innerHeight() * 3 }, 500);
                 break;
             default:
                 break;
         };
+    });
+    dom.back_top.click(function () {
+        $('html,body').animate({ scrollTop: dom.section.innerHeight() * 0 }, 500);
     });
 });
